756f6aaa59878bea27e217589cafe374861466f3
Walid boufroune <81040703+walidboufroun@users.noreply.github.com>
2023-12-06 23:29:30 +0100

    Update file.py

--- file.py
+++ file.py
@@ -1,1 +1,30 @@
-print('Hello World ! ')
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# Active l'utilisation de la sortie CGI
+print("Content-type: text/html\n\n")
+
+# Votre code Python ici
+result = "Hello, world from Python!"
+
+# Utilisez les balises de format pour inclure le résultat dans le HTML
+print(f"""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HTML + Python</title>
+</head>
+<body>
+    <h1>Bienvenue sur ma page HTML + Python</h1>
+    
+    <p>Le résultat du script Python :</p>
+    
+    <script>
+        // Affiche la sortie du script Python
+        document.write('<p>' + 'Output: ' + '<strong>' + '{result}' + '</strong>' + '</p>');
+    </script>
+</body>
+</html>
+""")

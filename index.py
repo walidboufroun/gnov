be3915cf678ffa6e007c35693f7f2a6b42da0d90
Walid boufroune <81040703+walidboufroun@users.noreply.github.com>
2023-12-06 23:26:21 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Page with PyScript</title>
+    <script src="https://cdn.jsdelivr.net/npm/pyscript"></script>
+</head>
+<body>
+    <h1>Hello World - SITE GNOV - </h1>
+    <div id="output"></div>
+
+    <script pyscript>
+        def say_hello():
+            print("Hello, world!")
+            document.getElementById("output").innerHTML = "Python said: Hello, world!"
+
+        say_hello()
+    </script>
+
+    <h2>Test site :  <script pyscript> print("Hey i'm code python") </script> </h2>
+</body>
+</html>
 
